1d89a88607aa551a303b996abc2fcce682f0623f
TonyKioko <tonykioko384@gmail.com>
2019-01-06 12:51:10 +0300

    now adding ingredients

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BuildControl.css';
+
+const buildConrol = (props) => (
+  <div className="BuildControl">
+  <div className="Label">{props.label}</div>
+  <button className="More" onClick={props.added}>More</button>
+  <button className="Less" onClick={props.removed} disabled={props.disabled} >Less</button>
+
+  </div>
+)
+
+export default buildConrol;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,82 @@
+import React,{Component} from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad:0.5,
+  bacon:0.4,
+  cheese:1.3,
+  meat: 0.7
+}
+
+class BurgerBuilder extends Component {
+
+  state = {
+    ingredients :{
+      salad:0,
+      bacon:0,
+      cheese:0,
+      meat: 0
+
+    },
+    totalPrice:4
+  }
+addIngredientsHandler = (type) => {
+  const oldCount = this.state.ingredients[type]
+  const updatedCount = oldCount + 1
+  const updateIngredients = {
+    ...this.state.ingredients
+  }
+  console.log("Adding")
+  updateIngredients[type] = updatedCount
+  const priceAddition = INGREDIENT_PRICES[type]
+  const oldPrice = this.state.totalPrice
+  const newPrice = priceAddition + oldPrice
+  this.setState({
+    totalPrice:newPrice,
+    ingredients:updateIngredients
+  })
+}
+removeIngredientsHandler = (type) => {
+  const oldCount = this.state.ingredients[type]
+if (oldCount <= 0) {
+return
+}
+
+  const updatedCount = oldCount - 1
+  const updateIngredients = {
+    ...this.state.ingredients
+  }
+  console.log("Adding")
+  updateIngredients[type] = updatedCount
+  const priceDeduction = INGREDIENT_PRICES[type]
+  const oldPrice = this.state.totalPrice
+  const newPrice = priceDeduction - oldPrice
+  this.setState({
+    totalPrice:newPrice,
+    ingredients:updateIngredients
+  })
+}
+
+  render() {
+    const disabledInfo ={
+      ...this.state.ingredients
+    }
+    for( let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    return (
+      <Aux>
+<Burger ingredients={this.state.ingredients}/>
+<BuildControls ingredientAdded={this.addIngredientsHandler} ingredientRemoved={this.removeIngredientsHandler} disabled={disabledInfo}
+price={this.state.totalPrice}
+   />
+
+      </Aux>
+    )
+  }
+}
+
+
+export default BurgerBuilder;
